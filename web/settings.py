78b2d768539def9f84eee25fcdcbf91b0f0ef602
Acheron <acheron.igor.b@gmail.com>
2017-01-09 02:47:45 +0200

    move static files again

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'dw_app/static'),
 )
 
 
